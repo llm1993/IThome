4931830e3f174239c0dee1f6724dc8c5c5a60650
llm1993 <halo877626533@gmail.com>
2016-10-20 08:33:19 +0800

    update

--- app/src/main/java/com/llm/myapplication/utils/Utils.java
+++ app/src/main/java/com/llm/myapplication/utils/Utils.java
@@ -20,13 +20,113 @@ import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+
 /**
  * Created by SAMSUNG on 2016/9/25.
+ * GET http://api.ithome.com/xml/newslist/maxnewsid.xml 获取最新的文章id
+
+ GET http://api.ithome.com/xml/newslist/news.xml?r=1474978735185 最新列表
+
+ GET http://api.ithome.com/xml/newslist/news_75b9e75b220e9192.xml?r=1474979005762 根据id取出50条
+
+ GET http://api.ithome.com/xml/newscontent/260/899.xml?r=1474978227754  获取文章内容
+
+ GET http://api.ithome.com/json/tags/0260/260899.json 相关文章
+
+
+ GET http://api.ithome.com/json/hotcommentlist/265/1f850e0010530a8f.json?r=1474978227757 获取热门评论
+ GET http://www.ithome.com/json/commentlist/265/1f850e0010530a8f.json?r=1476919608657 获取最新评论
+ GET http://www.ithome.com/json/commentlist/1f850e0010530a8f_19654999.json?r=1476919819261 根据上面评论最后id获取评论
+
  */
 
 public class Utils {
+    // 评论所需key
+    private final static byte[] commentKey = {-14, -7, -77, -102, -94, -16, -74, -1};
+    // 最新列表所需key
+    private final static byte[] newslistKey = {-86, -7, -69, -102, -83, -124, -87, -14};
     public static Map map = new HashMap();
     public static int type = 0;
+
+    public static String getNewsXmlList(String id) {
+        String urlStr = null;
+        if (id == null) {
+            urlStr = "http://api.ithome.com/xml/newslist/news.xml" + "?r=" + System.currentTimeMillis();
+        } else {
+            urlStr = "http://api.ithome.com/xml/newslist/news_" + getKeyByID(id, 1) + ".xml?r=" + System.currentTimeMillis();
+        }
+        return getRespByHttp(urlStr);
+    }
+
+    private static String getRespByHttp(String urlStr) {
+        StringBuilder sb = new StringBuilder();
+        try {
+            URL url = new URL(urlStr);
+            HttpURLConnection httpUrlConnection = (HttpURLConnection) url
+                    .openConnection();
+            httpUrlConnection.setRequestMethod("GET");
+            BufferedReader bfr = new BufferedReader(new InputStreamReader(
+                    httpUrlConnection.getInputStream(), "utf-8"));
+            String line;
+            while ((line = bfr.readLine()) != null) {
+                sb.append(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+        return sb.toString().trim();
+    }
+
+    public static String getCommentJsonList(String id) {
+        String urlStr = null;
+        if (id == null) {
+            urlStr = "http://api.ithome.com/xml/newslist/news.xml" + "?r=" + System.currentTimeMillis();
+        } else {
+            urlStr = "http://api.ithome.com/xml/newslist/news" + getKeyByID(id, 2) + ".xml?r=" + System.currentTimeMillis();
+        }
+        return getRespByHttp(urlStr);
+    }
+
+    public static String getKeyByID(String id, int keyType) {
+        String newsID = id + "\u0000\u0000";
+        byte[] key = null;
+        if (keyType == 1)
+            key = newslistKey;
+        if (keyType == 2)
+            key = commentKey;
+        byte[] arrayOfByte = new byte[key.length];
+        int i = 0;
+        while (i < key.length) {
+            arrayOfByte[i] = (byte) (-38 ^ key[i]);
+            i += 1;
+        }
+        SecretKeySpec secretKeySpec = new SecretKeySpec(arrayOfByte, "DES");
+        StringBuffer sb = null;
+        try {
+            Cipher cipher = Cipher.getInstance("DES/ECB/NoPadding");
+            cipher.init(1, secretKeySpec);
+            byte[] paramArrayOfByte = cipher.doFinal(newsID.getBytes());
+            sb = new StringBuffer();
+            i = 0;
+            while (i < paramArrayOfByte.length) {
+
+                String str = Integer.toHexString(paramArrayOfByte[i] & 0xFF);
+                if (str.length() == 1)
+                    sb.append("0" + str);
+                else
+                    sb.append(str);
+                i += 1;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+        return sb.toString();
+    }
+
 
     public static ArrayList getData(int page) {
         ArrayList arrayList = new ArrayList();
@@ -143,7 +243,6 @@ public class Utils {
         }
         return arrayList;
     }
-
 
     public static boolean isNetworkConnected(Context context) {
         if (context != null) {
