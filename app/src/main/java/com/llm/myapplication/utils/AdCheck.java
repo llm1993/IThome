ad494a4477456ea01ec87e8fdf48c84dc59a8c07
llm1993 <halo877626533@gmail.com>
2016-10-20 16:19:54 +0800

    create

---
+++ app/src/main/java/com/llm/myapplication/utils/AdCheck.java
@@ -0,0 +1,14 @@
+package com.llm.myapplication.utils;
+
+import com.llm.beans.NewsBean;
+
+public class AdCheck {
+	/**
+	 * 
+	 * @param bean
+	 * @return 检查这条新闻是否是广告 有：true 没有：false
+	 */
+	public static boolean checkHasAd(NewsBean bean) {
+		return false;
+	}
+}
