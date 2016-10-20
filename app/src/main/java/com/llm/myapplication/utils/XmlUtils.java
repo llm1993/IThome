361865f214e5535fe4deaceea214667cc21ac293
llm1993 <halo877626533@gmail.com>
2016-10-20 16:18:15 +0800

    create

---
+++ app/src/main/java/com/llm/myapplication/utils/XmlUtils.java
@@ -0,0 +1,119 @@
+package com.llm.myapplication;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import com.llm.beans.ContentBean;
+import com.llm.beans.NewsBean;
+
+public class XmlUtils {
+	
+	private static NewsBean beanthis;
+	public static List<NewsBean> getListBeans(String newsXmlUrl) {
+
+		List<NewsBean> list;
+		if (newsXmlUrl == null || newsXmlUrl.equals("")) {
+			return null;
+		} else {
+			list = parser(newsXmlUrl);
+		}
+		return list;
+	}
+	public static void setContent(NewsBean bean) {
+		beanthis = bean;
+		String url = "http://api.ithome.com/xml/newscontent/" + bean.getNewsID().substring(0, 3) + "/"
+				+ bean.getNewsID().substring(3) + ".xml?r=" + System.currentTimeMillis();
+		parser(url);
+	}
+
+	private static List parser(String newsXmlUrl) {
+		List list = new ArrayList<>();
+		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+		try {
+			DocumentBuilder db = dbf.newDocumentBuilder();
+			Document document = db.parse(newsXmlUrl);
+			NodeList root = document.getChildNodes();
+			Node channel = root.item(0).getChildNodes().item(1);
+			NodeList items = channel.getChildNodes();
+			for (int j = 0; j < items.getLength(); j++) {
+				Node item = items.item(j);
+				if (item.hasChildNodes()) {
+					NodeList nodebeans = item.getChildNodes();
+					NewsBean newsbean = new NewsBean();
+					ContentBean contentBean = new ContentBean();
+					for (int index = 0; index < nodebeans.getLength(); index++) {
+						Node nodebean = nodebeans.item(index);
+						if (nodebean.getNodeType() == 1) {
+							String content = nodebean.getTextContent();
+							switch (nodebean.getNodeName()) {
+							case "newsid":
+								newsbean.setNewsID(content);
+								break;
+							case "title":
+								newsbean.setTitle(content);
+								break;
+							case "c":
+								newsbean.setColor(content);
+								break;
+							case "url":
+								newsbean.setUrl(content);
+								break;
+							case "postdate":
+								newsbean.setPostDate(content);
+								break;
+							case "image":
+								newsbean.setImgUrl(content);
+								break;
+							case "description":
+								newsbean.setDescription(content);
+								break;
+							case "hitcount":
+								newsbean.setHitCount(content);
+								break;
+							case "commentcount":
+								newsbean.setCommentcount(content);
+								break;
+							case "forbidcomment":
+								newsbean.setForbidcomment(content.equals("true"));
+								break;
+							case "cid":
+								newsbean.setCid(content);
+								break;
+							case "newssource":
+								contentBean.setNewssource(content);
+								break;
+							case "newsauthor":
+								contentBean.setNewsauthor(content);
+								break;
+							case "detail":
+								contentBean.setDetail(content);
+								break;
+							case "z":
+								contentBean.setZ(content);
+								break;
+							default:
+								break;
+							}
+						}
+					}
+					if (newsbean.getNewsID() != null && !AdCheck.checkHasAd(newsbean)) {
+						list.add(newsbean);
+					}
+					if(contentBean.getDetail()!=null){
+						beanthis.setContent(contentBean);
+					}
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+		return list;
+	}
+}
